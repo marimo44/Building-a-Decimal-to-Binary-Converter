f7d943c5558aac78a1479086d472dffb56b58f13
Maria Amaba <142577342+marimo44@users.noreply.github.com>
2024-02-07 17:24:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
 
+const countDownAndUp = (number) => {
+  console.log(number);
 
-const a = () => {
-    return "freeCodeCamp " + b();
-  };
-  
-  const b = () => {
-    return "is " + c();
-  };
-  
-  const c = () => {
-    return "awesome!";
-  };
-  
-  console.log(a());
-  
-  const numberInput = document.getElementById("number-input");
-  const convertBtn = document.getElementById("convert-btn");
-  const result = document.getElementById("result");
-  
-  const decimalToBinary = (input) => {
-    let binary = "";
-  
-    if (input === 0) {
-      binary = "0";
-    }
-  
-    while (input > 0) {
-      binary = (input % 2) + binary;
-      input = Math.floor(input / 2);
-    }
-  
-    result.innerText = binary;
-  };
-  
-  const checkUserInput = () => {
-    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
-      alert("Please provide a decimal number");
-      return;
-    }
-  
-    decimalToBinary(parseInt(numberInput.value));
-    numberInput.value = "";
-  };
-  
-  convertBtn.addEventListener("click", checkUserInput);
-  
-  numberInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      checkUserInput();
-    }
-  });
+  if (number === 0) {
+    console.log("Reached base case");
+    return;
+  } else {
+    countDownAndUp(number - 1);
+    console.log(number);
+  }
+};
+
+countDownAndUp(3);
+
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
